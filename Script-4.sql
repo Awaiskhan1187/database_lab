fec454d3e1ab273dd588d47747dd4714147eeafc
Awaiskhan1187 <Awaiskhanpirpiai@gmail.com>
2026-01-29 11:18:07 +0500

    making schema of the data base

--- Script-4.sql
+++ Script-4.sql
@@ -1,2 +1,11 @@
 
-create table 
+create table if not exists Book(
+book_title varchar(50) not null,
+Author_name varchar(50),
+Category varchar(50),
+Number_of_pages INT,
+Date date,
+Rating INT
+);
+
+alter table book add column ID int primary key;
