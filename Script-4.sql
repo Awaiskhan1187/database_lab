f428de70fe59b525039ad36d406580ead6f5b1ff
Awaiskhan1187 <Awaiskhanpirpiai@gmail.com>
2026-02-01 21:06:17 +0500

    queries where i summeries data by group by method

--- Script-4.sql
+++ Script-4.sql
@@ -1,4 +1,3 @@
-
 CREATE table if not exists book(
 book_id SERIAL PRIMARY KEY,
 title VARCHAR(200) NOT NULL,
@@ -11,9 +10,46 @@ notes TEXT
 );
 
 insert into book(title,author,category,pages,date_finished,rating,notes)
-values('andhari raat k  musafir','naseem hijazi','History',300,'01-01-2021',4,'fall of span'),
-('aik aur bot shikn','naseem hijazi','History',300,'01-01-2021',4,'the qounquerer');
+values('andhari raat k  musafir','naseem hijazi','History',300,'2021-01-01',4,'fall of span'),
+('aik aur bot shikn','naseem hijazi','History',300,'2021-01-01',4,'the qounquerer');
 
+
+create table if not exists members(
+id int primary key,
+name varchar(50) not null,
+book_id int,
+Foreign key(book_id) references book(book_id),
+return varchar(10),
+issue_date date
+);
+
+
+insert into members(name,book_id,return,issue_date)
+values (
+'Awais Khan',
+1,
+'',
+'2021-01-01'
+);
+
+create sequence members_id_seq;
+alter table members
+alter column id set default nextval('members_id_seq' + id);
 select * from book;
 
+select name, count(name) as count
+from members
+group by name;
+
+select * from members;
+
+insert into members(name,book_id,return,issue_date) values('Awais Khan',
+4,
+'',
+'2021-01-01');
+
+insert into members(name,book_id,return,issue_date) values('Ayyan Khan',
+4,
+'',
+'2021-01-01');
 
