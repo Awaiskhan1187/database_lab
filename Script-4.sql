38842e64d7382599ad38866f347808dbd4c404bf
Awaiskhan1187 <Awaiskhanpirpiai@gmail.com>
2026-02-22 11:41:18 +0500

    Ecommerce database that store customer order details

--- Script-4.sql
+++ Script-4.sql
@@ -1,3 +1,4 @@
+
 CREATE table if not exists book(
 book_id SERIAL PRIMARY KEY,
 title VARCHAR(200) NOT NULL,
@@ -13,9 +14,8 @@ insert into book(title,author,category,pages,date_finished,rating,notes)
 values('andhari raat k  musafir','naseem hijazi','History',300,'2021-01-01',4,'fall of span'),
 ('aik aur bot shikn','naseem hijazi','History',300,'2021-01-01',4,'the qounquerer');
 
-
 create table if not exists members(
-id int primary key,
+id SERIAL primary key,
 name varchar(50) not null,
 book_id int,
 Foreign key(book_id) references book(book_id),
@@ -27,14 +27,14 @@ issue_date date
 insert into members(name,book_id,return,issue_date)
 values (
 'Awais Khan',
-1,
+4,
 '',
 '2021-01-01'
 );
 
-create sequence members_id_seq;
-alter table members
-alter column id set default nextval('members_id_seq' + id);
+
+
+
 select * from book;
 
 select name, count(name) as count
@@ -52,4 +52,3 @@ insert into members(name,book_id,return,issue_date) values('Ayyan Khan',
 4,
 '',
 '2021-01-01');
-
